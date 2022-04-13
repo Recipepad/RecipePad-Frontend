c5c772bbf71c3bef115cd1d3a403d30224b96c8a
EddieYuan666 <zy83@cornell.edu>
2022-04-13 15:02:11 -0400

    Create FileUpload.js
    Create FileUpload.js to save the image uploaded into server

---
+++ src/components/FileUpload.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import Dropzone from 'react-dropzone';
+import PlusOutlined from '@ant-design/icons/PlusOutlined';
+import Axios from 'axios';
+
+function FileUpload(props) {
+  const [Images, setImages] = useState([]);
+  const onDrop = (files) => {
+    let formData = new FormData();
+    const config = {
+      header: { 'content-type': 'multipart/form-data' },
+    };
+    formData.append('file', files[0]);
+
+    //Save the image chosen inside the Flask Server
+    Axios.post('/recipe/uploadImage', formData, config).then(
+      (response) => {
+        if (response.data.success) {
+          setImages([...Images, response.data.fileName]);
+          props.refreshFunction([...Images, response.data.fileName]);
+        } else {
+          alert('Failed to save the Image in Server');
+        }
+      }
+    );
+  };
+
+  const onDelete = (image) => {
+    const currentIndex = Images.indexOf(image);
+
+    let newImages = [...Images];
+    newImages.splice(currentIndex, 1);
+
+    setImages(newImages);
+    props.refreshFunction(newImages);
+  };
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+      <Dropzone onDrop={onDrop} multiple={false} maxSize={800000000}>
+        {({ getRootProps, getInputProps }) => (
+          <div
+            style={{
+              width: '300px',
+              height: '240px',
+              border: '1px solid lightgray',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center'
+            }}
+            {...getRootProps()}
+          >
+            <input {...getInputProps()} />
+            <PlusOutlined type='plus' style={{ fontSize: '3rem' }} />
+          </div>
+        )}
+      </Dropzone>
+
+      <div
+        style={{
+          display: 'flex',
+          width: '350px',
+          height: '240px',
+          overflowX: 'scroll',
+        }}
+      >
+        {Images.map((image, index) => (
+          <div onClick={() => onDelete(image)}>
+            <img
+              style={{ minWidth: '300px', width: '300px', height: '240px' }}
+              src={`/uploads/${image}`}
+              alt={`recipeImg-${index}`}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default FileUpload;
