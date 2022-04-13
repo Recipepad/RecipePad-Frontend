1c3fdb23ae86554555b025315796804868792a4a
EddieYuan666 <zy83@cornell.edu>
2022-04-13 15:06:18 -0400

    Create ImageSlider.js
    Create ImageSlider to show image of recipes on Landing page

---
+++ src/components/ImageSlider.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Carousel } from 'antd';
+
+function ImageSlider(props) {
+    return (
+        <div>
+            <Carousel autoplay>
+                {props.images.map((image, index) => (
+                    <div key={index}>
+                        <img style={{ width: '100%', maxHeight: '150px' }}
+                            src={`/uploads/${image}`} alt="recipeImage" />
+                    </div>
+                ))}
+            </Carousel>
+        </div>
+    )
+}
+
+export default ImageSlider
